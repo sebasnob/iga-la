e3a3e204ad8d1e84dc29fd5273f78d65a8043f95
Mori <morimartin@gmail.com>
2015-11-27 13:36:19 -0300

    arrow para subir al top

--- cursos.php
+++ cursos.php
@@ -364,7 +364,7 @@ if(isset($_GET['id_filial']) || isset($_SESSION['id_filial']))
                 </div>
             </div><!--/.row-->
         </section><!--/#single_cursos-->
-        
+        <div class="arrowTop"><i class="fa fa-arrow-circle-o-up"></i></div>
         <footer id="footer">
             <div class="footer-top wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="300ms">
                 <div class="container text-center">
