3ef788cd0265c2767a6ae9f627d289c54c03ad2e
Mori <morimartin@gmail.com>
2015-12-18 08:22:30 -0300

    modificaciones en menu y grilla

--- index.php
+++ index.php
@@ -208,10 +208,29 @@ $gridArrayCursos = getImagenesGrilla($mysqli, $_SESSION['idioma_seleccionado']['
         }
         ?>
         </header><!--/#home--> 
-        <section id="grillaCursos" class="portfolio">
-            <div class="container" id="grilla">
-                <div class="row">
-                    <?php 
+        <section id="grillaCursos" class="portfolio" style="display: inline-block;">
+            <div style="display: -webkit-box;">
+                <div class="col-md-1"></div>
+                <div id="1" class="col-md-2 textos_grilla" style="background-color: #2B933E;"><span><?=$lenguaje['gastro_'.$_SESSION['idioma_seleccionado']['cod_idioma']]?></span></div>
+                <div id="2" class="col-md-2 textos_grilla" style="background-color: #C40F79;"><span><?=$lenguaje['paste_avanzada_'.$_SESSION['idioma_seleccionado']['cod_idioma']]?></span></div>
+                <div id="3" class="col-md-2 textos_grilla" style="background-color: #246553;"><span><?=$lenguaje['certif_gastro_'.$_SESSION['idioma_seleccionado']['cod_idioma']]?></span></div>
+                <div id="4" class="col-md-2 textos_grilla" style="background-color: #F5B432;"><span><?=$lenguaje['cocineritos_'.$_SESSION['idioma_seleccionado']['cod_idioma']]?></span></div>
+                <div id="5" class="col-md-2 textos_grilla" style="background-color: #264699;"><span><?=$lenguaje['cursos_cortos_'.$_SESSION['idioma_seleccionado']['cod_idioma']]?></span></div>
+                <div class="col-md-1"></div>
+            </div>
+            <div style="display: -webkit-box;">
+                <div class="col-md-1"></div>
+                <div class="col-md-10" style="padding: 0">
+                    <?php
+                    $i = 1;
+                        foreach ($gridArrayCursos as $imgGrid){ ?>
+                            <img id="<?= 'grilla-'.$i ?>" class='img-responsive' <?php if($i > 1) {echo 'style="display:none;"';}?> src="<?=$imgGrid['img_url']?>">
+                        <?php $i++;} ?>
+                </div>
+                <div class="col-md-1"></div>
+            </div>
+        </section>    
+                <!--    <?php 
                     if(!$gridArrayCursos){
                         echo "<div class='text-center'>" . $lenguaje['no_existen_cursos_'.$_SESSION['idioma_seleccionado']['cod_idioma']] . "</div>";
                     }else{
@@ -244,7 +263,7 @@ $gridArrayCursos = getImagenesGrilla($mysqli, $_SESSION['idioma_seleccionado']['
                     }
                     ?>
                 </div>
-            </div> <!--/#container-fluid-porfolios-->
+            </div> /#container-fluid-porfolios-->
         </section><!--/#grillaCursos-->
         
         <section id="blog">
