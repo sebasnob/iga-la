1f504f391de0eecdd234f524a7a94aa55d2344a1
mmori <mmori@agrofy.com>
2015-12-22 10:48:57 -0300

    modificaciones en scrip de base de datos - le quito los inserts para dejar solo la estructura de la tabla grilla y remuevo los comentarios

--- grilla.sql
+++ grilla.sql
@@ -31,24 +31,13 @@ CREATE TABLE IF NOT EXISTS `grilla` (
   `img_url` text NOT NULL,
   `thumb_url` text NOT NULL,
   `prioridad` int(11) NOT NULL,
-  `cod_curso` int(11) NOT NULL COMMENT 'deberia ser clave foranea de cursos',
+  `cod_curso` int(11) NOT NULL,
   `habilitado` int(11) NOT NULL DEFAULT '1',
   `idioma` varchar(3) NOT NULL,
   `id_pais` text NOT NULL,
   `descripcion` text NOT NULL,
   `titulo` text NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `grilla`
---
-
-INSERT INTO `grilla` (`id`, `img_url`, `thumb_url`, `prioridad`, `cod_curso`, `habilitado`, `idioma`, `id_pais`, `descripcion`, `titulo`) VALUES
-(3, 'images/grilla/a71e6de1b2_chrysanthemum.jpg', 'images/grilla/thumb/a71e6de1b2_chrysanthemum.jpg', 2, 1, 1, 'es', '["1"]', 'Gastro y alta cocina brinda los conocimientos necesarios para alcanzar la excelencia como profesional de la gastronomía.', 'Gastro'),
-(6, 'images/grilla/16527663f6_desert.jpg', 'images/grilla/thumb/16527663f6_desert.jpg', 2, 102, 1, 'es', '["1"]', 'Gastro y alta cocina brinda los conocimientos necesarios para alcanzar la excelencia como profesional de la gastronomía.', ''),
-(7, 'images/grilla/e22924bd4f_jellyfish.jpg', 'images/grilla/thumb/e22924bd4f_jellyfish.jpg', 3, 81, 1, 'es', '["1"]', 'Gastro y alta cocina brinda los conocimientos necesarios para alcanzar la excelencia como profesional de la gastronomía.', ''),
-(8, 'images/grilla/ca3d43df0a_koala.jpg', 'images/grilla/thumb/ca3d43df0a_koala.jpg', 4, 29, 1, 'es', '["1"]', 'asd sad liasjdlk hjasldj aslkdjlkasjdlk a lkjaslk jd', ''),
-(9, 'images/grilla/efd4d811c4_lighthouse.jpg', 'images/grilla/thumb/efd4d811c4_lighthouse.jpg', 3, 17, 1, 'es', '["1"]', 'asd sad liasjdlk hjasldj aslkdjlkasjdlk a lkjaslk jd', '');
 
 --
 -- Índices para tablas volcadas
